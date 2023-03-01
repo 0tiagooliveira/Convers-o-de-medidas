41f7d23ca0b98ec65b5678536477d1be65f540ec
0tiagooliveira <123430824+0tiagooliveira@users.noreply.github.com>
2023-03-01 16:00:58 -0300

    Create Conversor de medidas.py

---
+++ Conversor de medidas.py
@@ -0,0 +1,5 @@
+v1 = float(input('Escreva quantos metros:'))
+centimetros = v1 * 100
+milimetros = v1 * 1000
+print(v1,'mm em centímetros é igual a:', centimetros, 'cm')
+print(v1,'cm em milímetros é igual a:', milimetros, 'mm')
